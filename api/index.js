8f1a62125c6300c8452553731f8509a1618bcb29
Sharif Hasan <me.sharif.hasan@gmail.com>
2024-02-01 14:39:02 +0600

    change package.json

--- api/index.js
+++ api/index.js
@@ -1,2 +1,15 @@
 import app from "../app";
+
+
+if (
+    process.env.LD_LIBRARY_PATH == null ||
+    !process.env.LD_LIBRARY_PATH.includes(
+        `${process.env.PWD}/node_modules/canvas/build/Release:`,
+    )
+) {
+    process.env.LD_LIBRARY_PATH = `${
+        process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 export default app;
--- app.js
+++ app.js
@@ -8,16 +8,7 @@ faceapi.env.monkeyPatch({ Canvas, Image });
 
 
 
-if (
-    process.env.LD_LIBRARY_PATH == null ||
-    !process.env.LD_LIBRARY_PATH.includes(
-        `${process.env.PWD}/node_modules/canvas/build/Release:`,
-    )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-      process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
+
 
 
 const app = express();
